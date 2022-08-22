153f41833fc1a7b9a90a6bfaac97c4e7781eddbd
mkhader96 <mkhader996@gmail.com>
2022-08-22 16:10:30 +0300

    Task2

--- README.md
+++ README.md
@@ -14,3 +14,21 @@ Also, in this app 5 props were used to pass data from Main component to HornedBe
 - Description
 
 The website was displayed including all those elements using the React library
+
+# Task 2
+
+In this task we used React Bootstrap to render the cards and grid.
+Used map function to get the data from JSON file.
+Used state to show a dynamic variable.
+
+I have an issue that the voting takes place on all the cards
+
+Number and name of feature: Voting and changing the state variable and using bootstrap
+
+Estimate of time needed to complete: 1.5 hours minutes
+
+Start time: 1:00 PM
+
+Finish time: 4:00 PM
+
+Actual time needed to complete: 3 hours
--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,16 +1,43 @@
-import React from 'react';
-
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import data from "./data.json";
 
 class HornedBeast extends React.Component {
-  render(){
-    return(
-    <div>
-      <h2>{this.props.title}</h2>
-      <img src={this.props.URL} alt={this.props.alt} title={this.props.imgTitle}></img>
-      <p>{this.props.Description}</p>
+  constructor(props) {
+    super(props); 
+    this.state = {
+        votes : 0
+    }
+}
 
-    </div>
-    )
+incrementVotes = () => {
+    this.setState({
+        votes : this.state.votes + 1
+    })
+}
+  render() {
+    return (
+      <Row className="row" >
+        <div  style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: 15 }}>
+          {data.map((jsonObject) => {
+            return (
+              
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={jsonObject.image_url} onClick={this.incrementVotes}/>
+                  <Card.Body>
+                    <Card.Title>{jsonObject.title}</Card.Title>
+                    <Card.Text>{jsonObject.description}</Card.Text>
+                    <Card.Text>Votes: {this.state.votes}</Card.Text>
+                  </Card.Body>
+                </Card>
+              
+            );
+          })}
+        </div>
+      </Row>
+    );
   }
 }
 
