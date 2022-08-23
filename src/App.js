46cbb7cbe729b27d0ddc968594a0eabf9ac7422c
mkhader96 <mkhader996@gmail.com>
2022-08-23 14:27:11 +0300

    Task 2 fixed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
-  render(){
-    return(
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-
-    </div>
-    )
-  }
+    render() {
+        return(
+            <div>
+                <Header />
+                <div>   
+                <Main />
+                </div>
+                <Footer />
+            </div>
+        )
+    }
 }
 
 export default App;
